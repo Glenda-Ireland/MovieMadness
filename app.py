6791f4dc05ca39e02bba82cb69b0fc3b0b8ad340
Glenda-Ireland <sba24460@student.cct.ie>
2025-05-23 01:10:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("Movie Madness")
 
 #loading the dataset 
 movies = pd.read_csv("https://raw.github.com/Glenda-Ireland/MovieMadness/main/movies.csv")
-similarity_with_item = pd.read_csv("https://raw.github.com/Glenda-Ireland/MovieMadness/main/similarity_with_item.csv", index=0)
+similarity_with_item = pd.read_csv("https://raw.github.com/Glenda-Ireland/MovieMadness/main/similarity_with_item.csv", index_col=0)
 
 valid_movie_ids = similarity_with_item.index.intersection(similarity_with_item.columns).intersection(movies["MovieId"])
 movies_filtered = movies[movies["movieId"].isin(valid_movie_ids)]
